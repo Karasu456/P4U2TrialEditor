97b9d7ee8640458960d0b53b73be8b7a6b088d8e
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2022-07-02 22:58:53 -0400

    Trying to embed resources

---
+++ UI/ActionTextBox.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4U2TrialEditor.UI
+{
+    public class ActionTextBox
+    {
+        // Base text box
+        private TextBox m_TextBox = new TextBox();
+        // Textbox background image
+        private PictureBox m_BackImage = new PictureBox();
+
+        public ActionTextBox()
+        {
+        }
+    }
+}
